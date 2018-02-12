b7178e87bbab4bf7a01bbd62c3f45257eebc2c97
sreejithgp <sreejith.gp@mobme.in>
2018-02-12 09:59:39 +0530

    1 week before data removed

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,46 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
+  var enjoyhint_instance = new EnjoyHint({});
+  var enjoyhint_script_steps = [
+    {
+      'click #search-form' : 'Search location for listing'
+    },  
+  ];
+  enjoyhint_instance.set(enjoyhint_script_steps);
+  if (!$("#search_location").val() && !localStorage.getItem('searched-location'))
+    enjoyhint_instance.run();
+  var enjoyhint_instance_tag = new EnjoyHint({});
+  var enjoyhint_tag_steps = [
+    {
+      'click .tags' : 'filter out based on tags'
+    },  
+  ];
+  enjoyhint_instance_tag.set(enjoyhint_tag_steps);
+  if ($(".tags h1").children().length > 0 && !localStorage.getItem('tags-found'))
+    enjoyhint_instance_tag.run();
+  var enjoyhint_instance_new = new EnjoyHint({});
+  var enjoyhint_new_steps = [
+    {
+      'click #note-1' : 'Create a new Add. Once submited an Add cannot be removed. It will be automatically removed after a week.'
+    },  
+  ];
+  if ($("#add-new").length > 0)
+    localStorage.setItem('new-add','true');
+  enjoyhint_instance_new.set(enjoyhint_new_steps);
+  if ( localStorage.getItem('searched-location') && localStorage.getItem('tags-found') && !localStorage.getItem('new-add'))
+    enjoyhint_instance_new.run();
   $("#search_location")
     .geocomplete()
     .bind("geocode:result", function (event, result) {
+      localStorage.setItem('searched-location','true');
       $("#search_latitude").val(result.geometry.location.lat());
       $("#search_longitude").val(result.geometry.location.lng());
       $("#search-form").submit();
     });
+
+  if ($(".tags h1").children().length > 0)
+    localStorage.setItem('tags-found','true');
   var containerW = 1100;
   var containerH = 750;
   var positions = [];
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @notices = Notice.near([params[:search_latitude], params[:search_longitude]], 150, units: :km, order: nil)
+    @notices.where("created_at < ?", (Time.now - 1.week)).delete_all
     @tags = ActsAsTaggableOn::Tag
       .where(id: ActsAsTaggableOn::Tagging
       .where(taggable_id: @notices.pluck(:id)).pluck(:tag_id)).pluck(:name)
