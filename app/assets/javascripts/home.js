3a85817f9f1404dba3a2221ebde2243011c7bfdf
sreejithgp <sreejith.gp@mobme.in>
2018-02-15 08:41:06 +0530

    js issue fix enjoyhint

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,35 +1,38 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-  var enjoyhint_instance = new EnjoyHint({});
-  var enjoyhint_script_steps = [
-    {
-      'click #search-form' : 'Search location for listing'
-    },  
-  ];
-  enjoyhint_instance.set(enjoyhint_script_steps);
-  if (!$("#search_location").val() && !localStorage.getItem('searched-location'))
+  if (!$("#search_location").val() && !localStorage.getItem('searched-location')){
+    var enjoyhint_instance = new EnjoyHint({});
+    var enjoyhint_script_steps = [
+      {
+        'click #search-form' : 'Search location for listing'
+      },  
+    ];
+    enjoyhint_instance.set(enjoyhint_script_steps);
     enjoyhint_instance.run();
-  var enjoyhint_instance_tag = new EnjoyHint({});
-  var enjoyhint_tag_steps = [
-    {
-      'click .tags' : 'filter out based on tags'
-    },  
-  ];
-  enjoyhint_instance_tag.set(enjoyhint_tag_steps);
-  if ($(".tags h1").children().length > 0 && !localStorage.getItem('tags-found'))
+  }
+  if ($(".tags h1").children().length > 0 && !localStorage.getItem('tags-found')){
+    var enjoyhint_instance_tag = new EnjoyHint({});
+    var enjoyhint_tag_steps = [
+      {
+        'click .tags' : 'filter out based on tags'
+      },  
+    ];
+    enjoyhint_instance_tag.set(enjoyhint_tag_steps);
     enjoyhint_instance_tag.run();
-  var enjoyhint_instance_new = new EnjoyHint({});
-  var enjoyhint_new_steps = [
-    {
-      'click #note-1' : 'Create a new Add. Once submited an Add cannot be removed. It will be automatically removed after a week.'
-    },  
-  ];
+  }
   if ($("#add-new").length > 0)
     localStorage.setItem('new-add','true');
-  enjoyhint_instance_new.set(enjoyhint_new_steps);
-  if ( localStorage.getItem('searched-location') && localStorage.getItem('tags-found') && !localStorage.getItem('new-add'))
+  if ( localStorage.getItem('searched-location') && localStorage.getItem('tags-found') && !localStorage.getItem('new-add')){
+    var enjoyhint_instance_new = new EnjoyHint({});
+    var enjoyhint_new_steps = [
+      {
+        'click #note-1' : 'Create a new Add. Once submited an Add cannot be removed. It will be automatically removed after a week.'
+      },  
+    ];
+    enjoyhint_instance_new.set(enjoyhint_new_steps);
     enjoyhint_instance_new.run();
+  }
   $("#search_location")
     .geocomplete()
     .bind("geocode:result", function (event, result) {
