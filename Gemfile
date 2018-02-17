0d76815d59ee7818617ccb8b37af83721894fbd3
sreejithgp <sreejith.gp@mobme.in>
2018-02-17 19:35:41 +0530

    figaro gem added

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'geocoder', '~> 1.4', '>= 1.4.5'
 gem 'kaminari'
 gem 'meta-tags'
 gem 'uglifier', '>= 1.3.0'
+gem 'figaro'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :rvm_use_path, '/etc/profile.d/rvm.sh'
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 # set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'public/')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', 'public/', 'config/application.yml')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
